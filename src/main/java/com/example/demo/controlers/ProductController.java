5e2016cce47c84b525f68e0efdaf154bae859117
goulkan <landongkista@email.unikom.ac.id>
2021-07-03 11:04:07 +0700

    contoh validasi

--- src/main/java/com/example/demo/controlers/ProductController.java
+++ src/main/java/com/example/demo/controlers/ProductController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controlers;
 
+import com.example.demo.dto.ResponseData;
 import com.example.demo.models.entities.Product;
 import com.example.demo.models.repos.ProductRepo;
 import com.example.demo.services.ProductService;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -15,34 +20,62 @@ public class ProductController {
 
     // TODO Controler memangil service, service memanggil controller
     @Autowired
-    private ProductRepo productRepo;
+    private ProductService productService;
 
     @PostMapping
-    public Product create(@RequestBody Product product) {
-        return productRepo.save(product);
+    public ResponseEntity<ResponseData<Product>> create(@Valid @RequestBody Product product, Errors errors) {
+
+        ResponseData<Product> responseData = new ResponseData<>();
+
+        if (errors.hasErrors()) {
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessage().add(error.getDefaultMessage());
+            }
+            responseData.setStatus(false);
+            responseData.setPayload(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        responseData.setStatus(true);
+        responseData.setPayload(productService.save(product));
+        return ResponseEntity.ok(responseData);
+
     }
 
     @GetMapping
     public Iterable<Product> findAll() {
-        return productRepo.findAll();
+        return productService.findAll();
     }
 
     @GetMapping("/{id}")
     public Product findeOne(@PathVariable("id") Long id) {
-        Optional<Product> product = productRepo.findById(id);
-        if (!product.isPresent()) {
-            return null;
-        }
-        return product.get();
+//        Optional<Product> product = productService.findOne(id);
+//        if (!product.isPresent()) {
+//            return null;
+//        }
+//        return product.get();
+        return productService.findOne(id);
     }
 
     @PutMapping
-    public Product update(@RequestBody Product product) {
-        return productRepo.save(product);
+    public ResponseEntity<ResponseData<Product>> update(@Valid @RequestBody Product product, Errors errors) {
+        ResponseData<Product> responseData = new ResponseData<>();
+
+        if (errors.hasErrors()) {
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessage().add(error.getDefaultMessage());
+            }
+            responseData.setStatus(false);
+            responseData.setPayload(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        responseData.setStatus(true);
+        responseData.setPayload(productService.save(product));
+        return ResponseEntity.ok(responseData);
+
     }
 
     @DeleteMapping("/{id}")
     public void deleteOne(@PathVariable("id") Long id) {
-        productRepo.deleteById(id);
+        productService.removeOne(id);
     }
 }
